7f540525a4d6b0a8c45b36ee72b66135f97a3b74
Antonio Nicolas Dianez Fernandez <andf1992@gmail.com>
2025-07-07 21:16:10 +0200

    Horizon

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Horizon\Horizon;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
+Horizon::auth(function ($request) {
+    return in_array($request->ip(), [
+        '47.61.40.68',
+    ]);
+});
